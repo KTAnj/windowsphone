0f8d30d95c8043eaab28d9614bbd989eaddef11a
KTAnj <thilininiroshi@gmail.com>
2014-04-22 17:08:42 +0530

    Add new button

--- PhoneApp1/PhoneApp1/MainPage.xaml.cs
+++ PhoneApp1/PhoneApp1/MainPage.xaml.cs
@@ -22,6 +22,11 @@ namespace PhoneApp1
             //BuildLocalizedApplicationBar();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world!");
+        }
+
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
         //{
